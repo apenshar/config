2d541f3385604fb06f3ba38a9c68333fe5ec4260
apenshar <apenshar@gmail.com>
2017-09-06 15:55:55 +0800

    add config file & script

---
+++ hello.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+Hello(){
+echo "HELOO WORLD $1 $2"
+}
+
+#Invoke yout function
+
+Hello ALEX Goulou
---
+++ test.js
@@ -0,0 +1,5 @@
+
+for (var i=1;i<10;i++){
+    var add = i => i+1;
+    var pow = i => i*i;
+    console.log(add(i),pow(i));}
